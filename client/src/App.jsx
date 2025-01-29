2ce514939ec6071bcc5c62e4deb10321d2f2733c
wajaht-ali <contact.chwajahat@gmail.com>
2025-01-29 15:14:28 +0500

    project initialization

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import "./styles/App.css";
+
+function App() {
+
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center mx-auto">
+      <h1 className="text-red-500 font-open-sans">Gen3D | Home Page</h1>
+    </div>
+  )
+}
+
+export default App;
+
+//[Vizcom tech stack docs]: https://neuralnotebook.substack.com/p/vizcom-ai-engine-turning-doodles
---
+++ client/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "open-sans": ["Open Sans"],
+        "dm-sans": ["DM Sans"],
+        montserrat: ["Montserrat"],
+      },
+      colors: {
+        primary: "#4f46e5",
+        light: "#fff",
+        lightShaded: "#f3f3f3",
+        dark: "#333",
+        darkShaded: "#1a1a1a",
+        danger: "#ef4444",
+        borderGray: "#9ca3af",
+        radius: "8px 0px 8px 0px",
+      },
+    },
+  },
+  plugins: [],
+};
